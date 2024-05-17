db503df170639661ccf68e1306447acfa4bb1d78
roberto <robertoramirezmoreno@gmail.com>
2024-05-17 07:07:21 -0300

    navbar fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,16 +5,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'My Laravel App')</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <!-- Puedes agregar más enlaces a hojas de estilo o scripts aquí -->
+    <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    
+    
+    
 </head>
 <body>
     <header>
-        <h1>Mi Aplicación Laravel</h1>
-        <!-- Aquí puedes agregar un menú de navegación si es necesario -->
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <span class="brand-word-1">Román</span> <span class="brand-word-2">Art</span>
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link nav-link-1" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link nav-link-2" href="#">Gallery</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link nav-link-3" href="#">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link nav-link-4" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        
     </header>
 
     <main>
+       
         @yield('content')
+        
     </main>
 
     <footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,10 @@
 @section('title', 'Home Page')
 
 @section('content')
+
     <h2>Bienvenido a la Página Principal</h2>
     <p>Este es el contenido de la página principal.</p>
+
+
+</div>
 @endsection
